474fdc967482092712eccdee6b6a5c8bb4be1712
Kailash Sharma <kailash_kds@yahoo.com>
2023-04-06 00:54:20 +0530

    Initial commit with: Meili search query Filters Sort

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -7,14 +7,28 @@ use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Meilisearch\Bundle\SearchService;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(SearchService $searchService, EntityManagerInterface $em, Request $request): Response
     {
+        $searchQuery = $request->query->get('q') ?? '';
+        $filter = $request->query->get('filters')?? "";
+        $filterData = [];
+        $rawFilter = json_decode($filter,true);
+
+        // Filters
+        // $movies = $searchService->search($em, Movie::class, $searchQuery, ['filter' => 'year > 1894 AND year < 1896','sort' => ['year:asc']]);
+        // sort
+        $movies = $searchService->search($em, Movie::class, $searchQuery, ['sort' => ['year:desc']]);
+
         return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
+            'movies' => $movies
         ]);
     }
 
