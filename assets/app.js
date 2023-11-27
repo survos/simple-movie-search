ad650c5cdce3f58a5dc0939d6c3a359849fd45fc
Tac Tacelosky <tacman@gmail.com>
2023-11-27 10:18:33 -0500

    update to symfony 6.4

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,8 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+// start the Stimulus application
+import './bootstrap.js';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
-// start the Stimulus application
-import './bootstrap';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
